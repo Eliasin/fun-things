65df6b6f99df3f1a2b4707548d98125027ad1b45
Steven Pham <steven.pham.8.2000@gmail.com>
2017-10-23 15:52:11 -0400

    Reorder nodes to inprove readability

--- recursion/hard.py
+++ recursion/hard.py
@@ -13,11 +13,11 @@ def dfs(adjacency, current, goal):
   _dfs(adjacency, visited, current, goal)
   return goal in visited
   
-#Represents a graph with 4 nodes, 0 - 3 where 0 and 1 are connected to 2 but nothing is connected to 3 
-adjacency = [[0, -1, 1, -1], [-1, 0, 1, -1], [1, 1, 0, -1], [-1, -1, -1, 0]]
+#Represents a graph with 4 nodes, 0 - 3 where 0 and 2 are connected to 1 but nothing is connected to 3 
+adjacency = [[0, 1, -1, -1], [1, 0, 1, -1], [-1, 1, 0, -1], [-1, -1, -1, 0]]
 
-#Can you reach 1 from 0?
-print(dfs(adjacency, 0, 1))
+#Can you reach 2 from 0?
+print(dfs(adjacency, 0, 2))
 
 #Can you reach 3 from 0?
 print(dfs(adjacency, 0, 3))
