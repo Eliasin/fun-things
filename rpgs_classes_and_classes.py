5862d90a7930127d969d23cf151a184706577241
Steven Pham <steven.pham.8.2000@gmail.com>
2017-12-11 10:08:22 -0500

    Update rpgs_classes_and_classes.py

--- rpgs_classes_and_classes.py
+++ rpgs_classes_and_classes.py
@@ -1,8 +1,27 @@
+class UnfortunateAdventurer:
+  def __init__(self, name: str) -> None:
+    self.name = name
+  
+  def smack(damage: int) -> None:
+    print("Ow ({})".format(damage))
+    
+  def slow(amount: int) -> None:
+    print("Oh noes, I am the slows ({})".format(amount))
+    
+  def cripple() -> None:
+    print("Oh no, I am now robbed of the use of my legs!")
+    
+  def magic_smack(magicalness: int) -> None:
+    print("This is too magical for me, my mind cannot handle the {} magical units of magicalness!".format(magicalness))
+    
+larry = UnfortunateAdventurer("Larry")
+
 #Common behaviour requires a lot of copy pasting/similar code
 class Sword:
-  def __init__(self, name: str, damage: int) -> None:
+  def __init__(self, name: str, damage: int, callout: str) -> None:
     self.name = name
     self.damage = damage
+    self.callout = callout
   
   def get_name(self) -> str:
     return self.name
@@ -10,11 +29,12 @@ class Sword:
   def get_damage(self) -> int:
     return self.damage
     
-  def action_callout(self) -> str:
-    return "IM A SWORD, SHWING SHWING! NOW YOU TAKE {} DAMAGE!".format(self.damage)
+  def use(self, target) -> str:
+    target.smack(self.damage)
+    return callout.format(self.damage)
 
 class Bow:
-  def __init__(self, name: str, damage: int) -> None:
+  def __init__(self, name: str, damage: int, callout: str) -> None:
     self.name = name
     self.damage = damage
   
@@ -24,25 +44,28 @@ class Bow:
   def get_damage(self) -> int:
     return self.damage
   
-  def action_callout(self) -> str:
-    return "IM A BOW, SWOOSH SWOOSH! NOW YOU TAKE {} DAMAGE!".format(self.damage)
+  def use(self, target) -> str:
+    target.smack(self.damage)
+    target.slow(self.damage // 2)
+    return callout.format(self.damage)
 
-longsword = Sword("Longsword", 8)
+longsword = Sword("Longsword", 8, "I'm a swooooord! Take {} damage!")
 
-print(longsword.action_callout())
+print(longsword.use(larry))
 
-shortbow = Bow("Shortbow", 6)
+shortbow = Bow("Shortbow", 6, "I'm a booow! Take {} damage and be slooowed by {}!)
 
-print(shortbow.action_callout())
+print(shortbow.use(larry))
 
 ########################################################################
 #Inheritance allows us to keep what is the same and change whatever is different,
 #meaning less copy pasting
 
 class MagicalItem:
-  def __init__(self, name: str, magicalness: int):
+  def __init__(self, name: str, magicalness: int, callout: str) -> None:
     self.name = name
     self.magicalness = magicalness
+    self.callout = callout
 
   def get_name(self) -> str:
     return self.name
@@ -50,27 +73,30 @@ class MagicalItem:
   def get_magicalness(self) -> int:
     return self.magicalness
   
-  def action_callout(self) -> str:
-    return "Im soooooooooooo magical look at me! I'm a {} that is {} magical units of magical if you care about the details.".format(self.name, self.magicalness)
+  def use(self, target) -> str:
+    return callout.format(self.name, self.magicalness)
     
 
 class Wand(MagicalItem):
-  def action_callout(self) -> str:
-    return "Im a waaaand! A {} that is so much lighter and better than other magical items! Im so good that I am {} magical units of magical.".format(self.name, self.magicalness)
+  def use(self, target) -> str:
+    target.magic_smack(self.magicalness)      
+    return callout.format(self.name, self.magicalness)
     
 class Staff(MagicalItem):
   def action_callout(self) -> str:
-    return "I'm a staff, a {}. I'm strong and dependable, and I'm {} magical units.".format(self.name, self.magicalness)
+    target.cripple()
+    target.magic_smack(self.magicalness)
+    return callout.format(self.name, self.magicalness)
     
 
-magic_mirror = MagicalItem("Magic Mirror", 10)
+magic_mirror = MagicalItem("Magic Mirror", 10, "Im soooooooooooo magical look at me! I'm a {} that is {} magical units of magical if you care about the details.")
 
-print(magic_mirror.action_callout())
+print(magic_mirror.use(larry))
 
-fire_wand = Wand("Fire Wand", 8)
+fire_wand = Wand("Fire Wand", 8, "Im a waaaand! A {} that is so much lighter and better than other magical items! Im so good that I am {} magical units of magical.")
 
-print(fire_wand.action_callout())
+print(fire_wand.use(larry))
 
-crystal_staff = Staff("Crystal Staff", 9)
+crystal_staff = Staff("Crystal Staff", 9, "I'm a staff, a {}. I'm strong and dependable, and I'm {} magical units.")
 
-print(crystal_staff.action_callout())
+print(crystal_staff.use(larry))
